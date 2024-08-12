76646eade17954881a3b298b3de92ff39dec3337
StefanieFelix <stefanie.seeck@gmx.de>
2024-08-12 14:30:27 +0200

    Colors/Theme

--- app/src/main/java/com/example/shadowwisper/MainActivity.kt
+++ app/src/main/java/com/example/shadowwisper/MainActivity.kt
@@ -11,37 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.shadowwisper.ui.theme.ShadowWisperTheme
+import com.example.shadowwisper.ui.theme.ExtendedColorScheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ShadowWisperTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShadowWisperTheme {
-        Greeting("Android")
     }
 }
