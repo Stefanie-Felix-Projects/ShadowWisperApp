9868d66ca4b17b48a1033e9cf47457a243bb133a
StefanieFelix <stefanie.seeck@gmx.de>
2024-08-15 18:01:04 +0200

    Zwischenstand 15.08.

--- app/src/main/java/com/example/shadowwisper/ui/theme/MainActivity.kt
+++ app/src/main/java/com/example/shadowwisper/ui/theme/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.example.shadowwisper.ui.theme
 
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import com.syntax_institut.whatssyntax.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Listener, für die Sichtbarkeit der BottomNavigationView zu steuern
+        binding.root.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                val r = Rect()
+                binding.root.getWindowVisibleDisplayFrame(r)
+                val screenHeight = binding.root.rootView.height
+                val keypadHeight = screenHeight - r.bottom
+
+                if (keypadHeight > screenHeight * 0.15) {
+                    //Tastatur sichtbar
+                    binding.navBottomBar.visibility = View.GONE
+                } else {
+                    // Tastatur unsichtbar
+                    binding.navBottomBar.visibility = View.VISIBLE
+                }
+            }
+
+        })
 
 
 
