5c6254dce6f0ed8fa067df8dc89cd57dc2479dcb
StefanieFelix <stefanie.seeck@gmx.de>
2024-08-15 13:27:44 +0200

    Zwischenstand 15.08.

--- app/src/main/java/com/example/shadowwisper/ui/theme/ui/StartseiteFragment.kt
+++ app/src/main/java/com/example/shadowwisper/ui/theme/ui/StartseiteFragment.kt
@@ -2,12 +2,6 @@ package com.example.shadowwisper.ui.theme.ui
 
 import androidx.fragment.app.Fragment
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StartseiteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StartseiteFragment : Fragment() {
     // TODO: Rename and change types of parameters
 
